1acc167ead92198a7139db6eee8c223460e89346
Ajax Lima <ajaxlima94@gmail.com>
2025-09-21 20:05:26 -0300

    refactor: adicionando __init__

--- src/infra/browser/BrowserFactory.py
+++ src/infra/browser/BrowserFactory.py
@@ -1,7 +1,5 @@
 from src.domain.model.Browser import Browser
-from src.infra.browser.Chrome import Chrome
-from src.infra.browser.Edge import Edge
-from src.infra.browser.Firefox import Firefox
+from src.infra.browser import Chrome, Edge, Firefox
 
 
 class BrowserFactory:
@@ -13,7 +11,7 @@ class BrowserFactory:
             "edge": Edge,
         }
 
-        _browser = browsers.get(browser.lower())
+        _browser: type[Browser] | None = browsers.get(browser.lower())
         if _browser is None:
             raise ValueError(f"Unsupported browser: {browser}")
 
---
+++ src/infra/browser/__init__.py
@@ -0,0 +1,4 @@
+from src.infra.browser.Chrome import Chrome
+from src.infra.browser.Edge import Edge
+from src.infra.browser.Firefox import Firefox
+from src.infra.browser.SeleniumBrowser import SeleniumBrowser
