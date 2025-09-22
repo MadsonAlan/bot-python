ed817960c05e82751cd7424826cea9d9138470d8
Ajax Lima <ajaxlima94@gmail.com>
2025-09-21 23:19:38 -0300

    feat: logica para ficar em loop passando as aulas quando forem concluidas

--- src/domain/usecases/abrir_curso.py
+++ src/domain/usecases/abrir_curso.py
@@ -1,3 +1,6 @@
+from logging import info
+from time import sleep
+
 from src.domain.model import Browser
 from src.pkg.settings import Settings, XpathSettings
 
@@ -14,3 +17,13 @@ def abrir_curso(browser: Browser, settings: Settings, xpaths: XpathSettings) -> 
     realizar_login(browser, settings, xpaths)
     browser.go_to(settings.url_base + settings.course_url)
     browser.click_button(xpaths.first_lesson_button)
+    while True:
+        while True:
+            info("Verifying lesson status")
+            if not browser.check_class_status():
+                break
+            info("Lesson completed")
+            browser.next_lesson()
+            sleep(1)
+        if not browser.check_class_status():
+            browser.play_video()
